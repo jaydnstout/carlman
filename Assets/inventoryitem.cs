67fd61f134ee3093b1363674ab6cb9d0017ea28d
MUKILTEO\1725440 <cray6975@gmail.com>
2025-10-02 13:25:11 -0700

    inv almost done

---
+++ Assets/inventoryitem.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class inventoryitem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("UI")]
+    public Image image;
+
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
+    }
+
+
+
+
+
+
+}
